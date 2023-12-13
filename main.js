ce10ed63b6e8defa0741c5cad1aa76aa340e9d2f
Luis Sousa <123762874+luissssousa@users.noreply.github.com>
2023-12-13 18:01:47 -0300

    Adding strong / Correcting text in JS

--- main.js
+++ main.js
@@ -12,7 +12,7 @@ const getHours = () =>{
     if(hour >=12 && hour < 18){
         saudacao.innerHTML = "Boa tarde"
     } else if (hour >= 18 && hour <23){
-        saudacao.innerHTML = "Boa Noite"
+        saudacao.innerHTML = "Boa noite"
     } else{
         saudacao.innerHTML = "Bom dia"
     }
